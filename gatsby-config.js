d56b14920f932e089274f657d6caa5f11bc26165
Gpx <polvara@gmail.com>
2019-06-16 14:45:54 +0200

    add open graph for posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     siteUrl: "https://www.polvara.me"
   },
   plugins: [
+    "gatsby-plugin-react-helmet",
     { resolve: "gatsby-plugin-styled-components", options: {} },
     {
       resolve: "gatsby-source-filesystem",
