dd5d5c40fe971e9018450d6e43cbecf26f6f0974
Gpx <polvara@gmail.com>
2023-04-11 16:10:42 +0200

    new design

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -1,7 +1,4 @@
 import React from "react";
-import Script from "next/script";
-import styles from "./Newsletter.module.scss";
-("");
 
 export default function Newsletter() {
   return (
@@ -10,10 +7,10 @@ export default function Newsletter() {
         src="https://dashboard.mailerlite.com/forms/367869/82797341317268730/share"
         width={400}
         height={285}
-        className={styles.frame}
+        className="mx-auto hidden rounded-lg border-4 border-black shadow-3d md:block"
       />
       <a
-        className={styles.link}
+        className="block rounded-lg border-4 border-black bg-pink-500 px-8 py-4 text-2xl font-black tracking-wide text-pink-50 shadow-3d transition-all hover:translate-x-0.5 hover:translate-y-0.5 hover:shadow-3d-hover active:translate-x-1 active:translate-y-1 active:shadow-none md:hidden"
         href="https://dashboard.mailerlite.com/forms/367869/82797341317268730/share"
         target="_blank"
       >
--- components/PostsList.js
+++ components/PostsList.js
@@ -1,19 +1,28 @@
+import cx from "classnames";
 import Link from "next/link";
-import styles from "./PostsList.module.scss";
 
-export default function PostsList({ posts, showMore = false }) {
+function Li({ children, href }) {
   return (
-    <ol className={styles.posts}>
+    <li>
+      <Link
+        href={href}
+        className="mx-auto my-8 block max-w-3xl rounded-lg border-4 border-black bg-rose-50 px-8 py-4 text-2xl font-bold text-rose-500 shadow-3d transition-all hover:translate-x-0.5 hover:translate-y-0.5 hover:shadow-3d-hover active:translate-x-1 active:translate-y-1 active:shadow-none"
+      >
+        {children}
+      </Link>
+    </li>
+  );
+}
+
+export default function PostsList({ posts, showMore = false, className }) {
+  return (
+    <ol className={cx("bg-rose-500 px-8 py-16", className)}>
       {posts.map((post) => (
-        <li key={post.slug}>
-          <Link href={`/posts/${post.slug}`}>{post.title}</Link>
-        </li>
+        <Li key={post.slug} href={`/posts/${post.slug}`}>
+          {post.title}
+        </Li>
       ))}
-      {showMore && (
-        <li>
-          <Link href="/blog">and more&hellip;</Link>
-        </li>
-      )}
+      {showMore && <Li href="/blog">and more&hellip;</Li>}
     </ol>
   );
 }
